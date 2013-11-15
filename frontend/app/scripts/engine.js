4d4b7b73aed75f24941bfc3ac69f692ca6a0d770
Michał Papierski <michal@papierski.net>
2013-11-15 22:14:01 +0100

    Engine double buffering

--- frontend/app/scripts/engine.js
+++ frontend/app/scripts/engine.js
@@ -4,15 +4,32 @@ engine.factory('engine', function() {
   return function() {
 
     var self = this;
-    self.objects = [];
+    
+    self.objects = [
+      {
+        x: 1, y: 1, w: 5, y: 5,
+        onDraw: function(ctx, td) {
+          ctx.fillStyle = "black";
+          ctx.fillRect(this.x, this.y, this.w, this.h);
+        },
+        onTick: function(ctx, td) {}
+    }];
+
     self.el = null; // canvas dom
+    self.createCanvas = function(width, height) {
+      var canvas = document.createElement('canvas');
 
-    self.init = function(e) {
-      self.el = e;
+      canvas.width = width;
+      canvas.height = height;
 
-      self.ctx = e.getContext('2d');
+      return canvas;
+    }
+    self.init = function(el) {
+      self.el = el;
+      self.ctx = self.el.getContext('2d');
+      self.buffer = self.createCanvas(640, 480);
+      self.bufferCtx = self.buffer.getContext('2d');
       window.requestAnimationFrame(self.loop);
-      console.log('jestem')
     }
     var old = null;
       
@@ -20,7 +37,6 @@ engine.factory('engine', function() {
       window.requestAnimationFrame(self.loop);
       if (old === null) {
         old = timestamp;
-        
         return;
       }
       self.objects.forEach(function(obj) {
@@ -28,6 +44,7 @@ engine.factory('engine', function() {
         obj.onTick(self.ctx, timestamp - old);
       });
       old = timestamp;
+     // self.ctx.drawImage(self.buffer,0, 0);
     }
   }
 });
