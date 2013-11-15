30b58456c98026ad30848cfff49dfd185b18663d
asdf <asdf@trewq.pl>
2013-11-16 00:38:04 +0100

    rtc stuff cleanup

--- frontend/app/scripts/app.js
+++ frontend/app/scripts/app.js
@@ -2,51 +2,6 @@
 
 
 
-
-var iceServers = {
-    iceServers: [{
-        url: 'stun:stun.l.google.com:19302'
-    }]
-};
-
-var optionalRtpDataChannels = {
-    optional: [{
-        RtpDataChannels: true
-    }]
-};
-
-
-var mediaConstraints = {
-    optional: [],
-    mandatory: {
-        OfferToReceiveAudio: false,
-        OfferToReceiveVideo: false
-    }
-};
-
-
-
-function setChannelEvents(channel, channelName) {
-    channel.onmessage = function (event) {
-        console.debug(channelName, 'received a message:', event.data);
-    };
-
-    channel.onopen = function () {
-        console.debug('channel', channelName, 'open');
-        channel.send('first text message over RTP data ports');
-    };
-    channel.onclose = function (e) {
-        console.error(e);
-    };
-    channel.onerror = function (e) {
-        console.error(e);
-    };
-}
-
-
-
-
-
 var app = angular.module('app', [
   'ngResource',
   'ui.router',
@@ -55,48 +10,14 @@ var app = angular.module('app', [
 ]);
 
 
-
-
-
 var MainCtrl = function($scope, server, client, $http, $state){
-  $scope.rtcdata = {
-    ice: [],
-    sdp: {}
-  };
-  $scope.client = null;
+  server($scope);
 
-  var serverChannel = server($scope);
-
-
-  $scope.$on('ice', function(evt, ice){
-    $scope.rtcdata.ice.push(ice);
-    sendData();
-    
-    $scope.$apply();
-  });
-
-  function sendData(){
-    $http({
-      method: 'POST',
-      url: '/',
-      data: JSON.stringify($scope.rtcdata)
-    }).success(function(data, status, headers, config) {
-      console.log('wyslalem');
-    });
-  }
-  $scope.$on('sd', function(evt, sdp){
-    sendData();
-    $scope.rtcdata.sdp = sdp;
-    $scope.$apply();
-  });
-
-  setChannelEvents(serverChannel, 'server');
   $scope.connect = function(){
     $scope.$broadcast('connect');
   };
+
 };
-
-
 
 var ClientCtrl = function($scope, client, $http){
   client($scope);
@@ -113,6 +34,11 @@ app.config(function($stateProvider){
     url: '/client',
     controller: ClientCtrl,
     template: '<div>client</div>'
+  });
+    $stateProvider.state('game',{
+    url: '/game',
+    controller: 'GameCtrl',
+    template: '<canvas screen></canvas>'
   });
 });
 
---
+++ frontend/app/scripts/rtc.js
@@ -0,0 +1,152 @@
+'use strict';
+var rtc = angular.module('rtc', []);
+
+
+
+
+var iceServers = {
+  iceServers: [{
+    url: 'stun:stun.l.google.com:19302'
+  }]
+};
+
+var optionalRtpDataChannels = {
+  optional: [{
+    RtpDataChannels: true
+  }]
+};
+
+
+var mediaConstraints = {
+  optional: [],
+  mandatory: {
+    OfferToReceiveAudio: false,
+    OfferToReceiveVideo: false
+  }
+};
+
+
+
+rtc.factory('client', function(){
+  return function createClient(scope) {
+      var answerer = new webkitRTCPeerConnection(iceServers, optionalRtpDataChannels);
+      var answererDataChannel = answerer.createDataChannel('RTCDataChannel', {
+          reliable: false
+      });
+      window.channel = answererDataChannel;
+      var ws = new WebSocket('ws://127.0.0.1:5001');
+
+      ws.onopen = function(){
+        console.log('open');
+      };
+
+      ws.onmessage = function(msg){
+        var data = JSON.parse(msg.data);
+        
+        if (data.type === 'sd'){
+          var sdp = new RTCSessionDescription(data.data);
+
+          answerer.setRemoteDescription(sdp);
+          answerer.createAnswer(function (sessionDescription) {
+              answerer.setLocalDescription(sessionDescription);
+              ws.send(JSON.stringify({
+                type: 'sd',
+                data: {
+                  type: sessionDescription.type,
+                  sdp: sessionDescription.sdp
+                }
+              }));
+          }, null, mediaConstraints); 
+        }
+        if (data.type === 'ice'){
+          answerer.addIceCandidate(new RTCIceCandidate(data.data));
+        }
+      };
+
+      
+      answererDataChannel.onopen = function(){
+        scope.channel = answererDataChannel;
+      }
+      
+      answerer.onicecandidate = function (event) {
+          if (!event || !event.candidate) return;
+          ws.send(JSON.stringify({
+            type: 'ice',
+            data: {
+              sdpMLineIndex:  event.candidate.sdpMLineIndex,
+              sdpMid: event.candidate.sdpMid,
+              candidate: event.candidate.candidate
+            }
+          }));
+      };
+
+  }
+
+
+});
+
+
+
+
+rtc.factory('server', function(){
+
+
+
+  return function createServer(scope){
+    var ws = new WebSocket('ws://127.0.0.1:5001');
+    var serverScope = scope.$new();
+    var server = new webkitRTCPeerConnection(iceServers, optionalRtpDataChannels);
+
+    var serverDataChannel = server.createDataChannel('RTCDataChannel', {
+      reliable: false
+    });
+    
+    ws.onmessage = function(msg){
+        var data = JSON.parse(msg.data);
+        if (data.type === 'sd'){
+          var sdp = new RTCSessionDescription(data.data);
+          server.setRemoteDescription(sdp);
+        }
+        if (data.type === 'ice'){
+          server.addIceCandidate(new RTCIceCandidate(data.data));
+        }
+    };
+
+    serverDataChannel.onopen = function(){
+      scope.channel = serverDataChannel;
+    }
+
+
+    server.onicecandidate = function (event) {
+      if (!event || !event.candidate){
+        return;
+      }
+      ws.send(JSON.stringify({
+        type: 'ice',
+        data: {
+          sdpMLineIndex:  event.candidate.sdpMLineIndex,
+          sdpMid: event.candidate.sdpMid,
+          candidate: event.candidate.candidate
+        }
+      }));
+    };
+
+
+    scope.$on('connect', function(){
+
+      server.createOffer(function (sessionDescription) {
+        server.setLocalDescription(sessionDescription);
+        ws.send(JSON.stringify({
+          type: 'sd',
+          data: {
+            type: sessionDescription.type,
+            sdp: sessionDescription.sdp
+          }
+        }));
+      }, null, mediaConstraints);
+
+    });
+
+  };
+
+});
