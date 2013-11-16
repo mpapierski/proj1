e299db2a88fbce05d5a05e250fc1d5a9b6788183
asdf <asdf@trewq.pl>
2013-11-16 01:22:20 +0100

    add backgrounds list to engine

--- frontend/app/scripts/engine.js
+++ frontend/app/scripts/engine.js
@@ -4,6 +4,7 @@ engine.factory('engine', function() {
   return function() {
 
     var self = this;
+    self.backgrounds = [];
     self.objects = [];
     self.el = null; // canvas dom
     self.createCanvas = function(width, height) {
@@ -17,9 +18,13 @@ engine.factory('engine', function() {
 
     self.init = function(el) {
       self.el = el;
-      self.ctx = self.el.getContext('2d');
       self.buffer = self.createCanvas(640, 480);
+      self.backgroundCanvas = self.createCanvas(640, 480);
+
+      self.backgroundCtx = self.backgroundCanvas.getContext('2d');
+      self.ctx = self.el.getContext('2d');
       self.bufferCtx = self.buffer.getContext('2d');
+
       window.requestAnimationFrame(self.loop);
     };
 
@@ -27,17 +32,27 @@ engine.factory('engine', function() {
     self.loop = function(timestamp) {
       self.buffer.width = self.buffer.width;
       self.el.width = self.el.width;
+      self.backgroundCanvas.width = self.backgroundCanvas.width;
+
       window.requestAnimationFrame(self.loop);
       if (old === null) {
         old = timestamp;
         return;
       }
+
+      self.backgrounds.forEach(function(obj) {
+        obj.onDraw(self.backgroundCtx, timestamp - old);
+      });
+
       self.objects.forEach(function(obj) {
         obj.onDraw(self.bufferCtx, timestamp - old);
         obj.onTick(self.bufferCtx, timestamp - old);
       });
-      old = timestamp;
+
+      self.ctx.drawImage(self.backgroundCanvas, 0, 0);
       self.ctx.drawImage(self.buffer, 0, 0);
+
+      old = timestamp;
     };
   }
 });
