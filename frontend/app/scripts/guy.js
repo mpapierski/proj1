cfce5baf5c7f0c750f4bca5a03e9a127acd91eea
asdf <asdf@trewq.pl>
2013-11-16 01:02:30 +0100

    guy anims

--- frontend/app/scripts/guy.js
+++ frontend/app/scripts/guy.js
@@ -1,3 +1,229 @@
+var idlePoints = [
+  { // chest
+    name: 'idle',
+    angle: 0,
+    length: 0,
+    children: [
+      {
+        name: 'neck',
+        angle: Math.PI,
+        length: 1.5,
+        children: [
+          { // head
+            name: 'head',
+            angle: Math.PI * 0.1,
+            length: 2
+          },
+        ]
+      },
+      { // head
+        name: 'body',
+        angle: 0,
+        length: 6,
+        children: [
+          {
+            name: 'right leg',
+            angle: Math.PI * 0.2,
+            length: 5,
+            children: [
+              {
+                angle: -Math.PI*0.2,
+                length: 3
+              }
+            ]
+          },
+          {
+            name: 'left leg',
+            angle: -Math.PI * 0.2,
+            length: 5,
+            children: [
+              {
+                angle: Math.PI*0.2,
+                length: 3
+              }
+            ]
+          }
+        ]
+      },
+      { // left arm
+        name: 'left arm',
+        angle: Math.PI * 0.5,
+        length: 5,
+        children: [
+          {
+            name: 'right hand',
+            angle: -Math.PI * 0.7,
+            length: 5
+          }
+        ]
+      },
+      { // right arm
+        name: 'right arm',
+        angle: -Math.PI * 0.5,
+        length: 5,
+        children: [
+          {
+            name: 'right hand',
+            angle: Math.PI * 0.7,
+            length: 5
+          }
+        ]
+      }
+    ]
+  }
+];
+
+
+var runRight1 = [
+  { // chest
+    name: 'runright1',
+    angle: 0,
+    length: 0,
+    children: [
+      {
+        name: 'neck',
+        angle: Math.PI,
+        length: 1.5,
+        children: [
+          { // head
+            name: 'head',
+            angle: Math.PI * 0.1,
+            length: 2
+          },
+        ]
+      },
+      { // head
+        name: 'body',
+        angle: 0,
+        length: 6,
+        children: [
+          {
+            name: 'right leg',
+            angle: -Math.PI * 0.2,
+            length: 5,
+            children: [
+              {
+                angle: -Math.PI*0.2,
+                length: 3
+              }
+            ]
+          },
+          {
+            name: 'left leg',
+            angle: Math.PI * 0.2,
+            length: 5,
+            children: [
+              {
+                angle: Math.PI*0.2,
+                length: 3
+              }
+            ]
+          }
+        ]
+      },
+      { // left arm
+        name: 'left arm',
+        angle: Math.PI * 0.5,
+        length: 5,
+        children: [
+          {
+            name: 'right hand',
+            angle: -Math.PI * 0.7,
+            length: 5
+          }
+        ]
+      },
+      { // right arm
+        name: 'right arm',
+        angle: -Math.PI * 0.5,
+        length: 5,
+        children: [
+          {
+            name: 'right hand',
+            angle: Math.PI * 0.7,
+            length: 5
+          }
+        ]
+      }
+    ]
+  }
+];
+var runRight2 = [
+  { // chest
+    name: 'runright2',
+    angle: 0,
+    length: 0,
+    children: [
+      {
+        name: 'neck',
+        angle: Math.PI,
+        length: 1.5,
+        children: [
+          { // head
+            name: 'head',
+            angle: -Math.PI * 0.1,
+            length: 2
+          },
+        ]
+      },
+      { // head
+        name: 'body',
+        angle: 0,
+        length: 6,
+        children: [
+          {
+            name: 'right leg',
+            angle: Math.PI * 0.2,
+            length: 5,
+            children: [
+              {
+                angle: -Math.PI*0.2,
+                length: 3
+              }
+            ]
+          },
+          {
+            name: 'left leg',
+            angle: -Math.PI * 0.2,
+            length: 5,
+            children: [
+              {
+                angle: Math.PI*0.2,
+                length: 3
+              }
+            ]
+          }
+        ]
+      },
+      { // left arm
+        name: 'left arm',
+        angle: Math.PI * 0.5,
+        length: 5,
+        children: [
+          {
+            name: 'right hand',
+            angle: -Math.PI * 0.7,
+            length: 5
+          }
+        ]
+      },
+      { // right arm
+        name: 'right arm',
+        angle: -Math.PI * 0.5,
+        length: 5,
+        children: [
+          {
+            name: 'right hand',
+            angle: Math.PI * 0.7,
+            length: 5
+          }
+        ]
+      }
+    ]
+  }
+];
+
+
 // jshint camelcase: false
 'use strict';
 
@@ -16,10 +242,19 @@ guyModule.factory('moveStates', function(){
     right: {
       onEnter: function(sender){
         sender.acc.x += sender.acc.factor * 1;
+        var counter = 0;
+        function x(){
+          var target = counter % 2 ? runRight1 : runRight2;
+          counter +=1;
+          sender.animate(target).then(x);
+        }
+        x();
       },
+
       onExit: function(sender){
         sender.acc.x -= sender.acc.factor * 1;
       },
+
       release_right: function(sender){
         sender.transitionTo('moving', 'idle');
       }
@@ -66,156 +301,6 @@ guyModule.factory('states', function(moveStates, hpStates){
 
 
 
-var idlePoints = [
-  { // chest
-    name: 'chest',
-    angle: 0,
-    length: 0,
-    children: [
-      {
-        name: 'neck',
-        angle: Math.PI,
-        length: 1.5,
-        children: [
-          { // head
-            name: 'head',
-            angle: Math.PI * 0.1,
-            length: 2
-          },
-        ]
-      },
-      { // head
-        name: 'body',
-        angle: 0,
-        length: 6,
-        children: [
-          {
-            name: 'right leg',
-            angle: Math.PI * 1.7,
-            length: 5,
-            children: [
-              {
-                angle: Math.PI*0.2,
-                length: 3
-              }
-            ]
-          },
-          {
-            name: 'left leg',
-            angle: -Math.PI * 1.7,
-            length: 5,
-            children: [
-              {
-                angle: Math.PI*0.2,
-                length: 3
-              }
-            ]
-          }
-        ]
-      },
-      { // left arm
-        name: 'left arm',
-        angle: Math.PI * 0.5,
-        length: 5,
-        children: [
-          {
-            name: 'right hand',
-            angle: -Math.PI * 0.7,
-            length: 5
-          }
-        ]
-      },
-      { // right arm
-        name: 'right arm',
-        angle: -Math.PI * 0.5,
-        length: 5,
-        children: [
-          {
-            name: 'right hand',
-            angle: Math.PI * 0.7,
-            length: 5
-          }
-        ]
-      }
-    ]
-  }
-];
-
-var points2 = [
-  { // chest
-    name: 'chest',
-    angle: 0,
-    length: 0,
-    children: [
-      {
-        name: 'neck',
-        angle: Math.PI,
-        length: 1.5,
-        children: [
-          { // head
-            name: 'head',
-            angle: Math.PI * 0.1,
-            length: 2
-          },
-        ]
-      },
-      { // head
-        name: 'body',
-        angle: 0,
-        length: 6,
-        children: [
-          {
-            name: 'right leg',
-            angle: Math.PI * 1.7,
-            length: 5,
-            children: [
-              {
-                angle: Math.PI*0.2,
-                length: 3
-              }
-            ]
-          },
-          {
-            name: 'left leg',
-            angle: -Math.PI * 1.7,
-            length: 5,
-            children: [
-              {
-                angle: Math.PI*0.2,
-                length: 3
-              }
-            ]
-          }
-        ]
-      },
-      { // left arm
-        name: 'left arm',
-        angle: Math.PI * 0.5,
-        length: 5,
-        children: [
-          {
-            name: 'right hand',
-            angle: -Math.PI * 0.7,
-            length: 5
-          }
-        ]
-      },
-      { // right arm
-        name: 'right arm',
-        angle: -Math.PI * 0.5,
-        length: 5,
-        children: [
-          {
-            name: 'right hand',
-            angle: Math.PI * 1.7,
-            length: 5
-          }
-        ]
-      }
-    ]
-  }
-];
-
 guyModule.factory('Guy', function($q, states, $timeout){
 
   return function Guy(){
@@ -232,30 +317,47 @@ guyModule.factory('Guy', function($q, states, $timeout){
 
     self.currentAnim = angular.copy(idlePoints);
     self.previousAnim = idlePoints;
-    self.targetAnim = points2;
+    self.targetAnim = idlePoints;
     self.animDone = 0;
-    function doAnimate(delta){
-      self.animDone += delta / 500;
-      function internal(currentAnim, previousAnim, targetAnim){
-        _.each(_.zip(currentAnim, previousAnim, targetAnim), function(objs){
-          var current = objs[0];
-          var previous = objs[1];
-          var target = objs[2];
-          current.length = previous.length * (1 - self.animDone) + target.length * self.animDone;
-          current.angle = previous.angle * (1 - self.animDone) + target.angle * self.animDone;
-          if (current.children){
-            internal(current.children, previous.children, target.children);
-          }
-        });
-      }
+    self.animDfd = null;
 
+    function animInternal(currentAnim, previousAnim, targetAnim, animDone){
+      _.each(_.zip(currentAnim, previousAnim, targetAnim), function(objs){
+        var current = objs[0];
+        var previous = objs[1];
+        var target = objs[2];
+        current.length = previous.length * (1 - self.animDone) + target.length * self.animDone;
+        current.angle = previous.angle * (1 - self.animDone) + target.angle * self.animDone;
+
+        if (current.children){
+          animInternal(current.children, previous.children, target.children, animDone);
+        }
+
+      });
+    }
+
+    function doAnimate(delta){
       
-      internal(self.currentAnim, self.previousAnim, self.targetAnim);
-      if (self.animDone >= 500){
+      self.animDone += delta / 500;
+
+      animInternal(self.currentAnim, self.previousAnim, self.targetAnim, self.animDone);
+      
+      if (self.animDone >= 1){
         self.currentAnim = self.targetAnim;
         self.previousAnim = self.targetAnim;
+        self.animDone = 0;
+        if (self.animDfd){
+          self.animDfd.resolve();
+        }
       }
+
     }
+
+    self.animate = function(targetAnim){
+      self.animDfd = $q.defer();
+      self.targetAnim = targetAnim;
+      return self.animDfd.promise;
+    };
 
     self.states = {
       moving: states.moving.idle,
@@ -292,7 +394,7 @@ guyModule.factory('Guy', function($q, states, $timeout){
     };
     self.onTick = function(ctx, timedelta){
       self.x += timedelta * self.acc.x;
-          // doAnimate(timedelta);
+      doAnimate(timedelta);
 
     };
 
